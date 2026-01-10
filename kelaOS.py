eef17db3e8bcf16230dc345310985b26e68375f4
k9gy67 <shedevroaboba@gmail.com>
2026-01-10 11:40:41 +0300

    Add files via upload

---
+++ kelaOS.py
@@ -0,0 +1,76 @@
+print('1:settings')
+print('2:off sistem')
+print('3:developer')
+print('4:username')
+print('5:notepad')
+print('6:OS settings')
+print('7:photo')
+print('8:messenger')
+print('9:kela store')
+print('10:calculator')
+print('11:app creator')
+
+while True:
+    user_input = input("> ").lower().strip()
+    
+    if user_input == "2":
+        print("goodbye!")
+        break
+    elif user_input in ["1", "1"]:
+        print("OS:kela,Theme:dark")
+    elif user_input in ["3", "3"]:
+        print("developer:k9gy")
+    elif user_input in ["4", "4"]:
+        input("enter username:")
+    elif user_input in ["5", "5"]:
+        input("enter text for notepad:")
+    elif user_input in ["6", "6"]:
+        print('delete OS?')
+    elif user_input in ["yes", "yes"]:
+        break
+    elif user_input in ["7", "7"]:
+        print('111111111111111111111111111111')
+        print('111111111111111111111111111111')
+        print('111111  1111111111111111  1111')
+        print('111111111111111111111111111111')
+        print('111111111111111111111111111111')
+        print('111111111111111111111111111111')
+        print('111111111111111111111111111111')
+        print('1111   1111111111111111   1111')
+        print('1111111                1111111')
+        print('111111111111111111111111111111')
+        print('111111111111111111111111111111')
+    elif user_input in ["8", "8"]:
+        input('enter nickname:')
+        input('enter message:')
+    elif user_input in ["9", "9"]:
+        print('s:song,v:vpn')
+    elif user_input in ["s", "s"]:
+        print('download...')
+        print('Last Christmas I gave you my heart But the very next day you gave it away This year To save me from tears I’ll give it to someone special Once bitten and twice shy I keep my distance But you still catch my eye Tell me baby Do you recognize me?')
+    elif user_input in ["v", "v"]:
+        print('download...')
+        print('vpn activated!')
+    elif user_input in ["10", "10"]:
+        print('answer:', eval(input()))
+    elif user_input in ["11", "11"]:
+        input("app name:")
+        print('app type')
+        print('vi:virus, ca:camera')
+    elif user_input in ["vi", "vi"]:
+        break
+    elif user_input in ["ca", "ca"]:
+        print('its you?')
+        print('111111111111111111111111111111')
+        print('111111111111111111111111111111')
+        print('111111  1111111111111111  1111')
+        print('111111111111111111111111111111')
+        print('111111111111111111111111111111')
+        print('111111111111111111111111111111')
+        print('111111111111111111111111111111')
+        print('1111   1111111111111111   1111')
+        print('1111111                1111111')
+        print('111111111111111111111111111111')
+        print('111111111111111111111111111111')
+    else:
+        print("please enter the number")
