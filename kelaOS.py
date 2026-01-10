f0c5acd722ef01126a87205ca9ec91bc73c5525a
k9gy67 <shedevroaboba@gmail.com>
2026-01-10 18:06:32 +0300

    kelaOS.py

--- kelaOS.py
+++ kelaOS.py
@@ -1,3 +1,5 @@
+t = input('enter username:')
+s = input('enter password:')
 print('1:settings')
 print('2:off sistem')
 print('3:developer')
@@ -9,6 +11,8 @@ print('8:messenger')
 print('9:kela store')
 print('10:calculator')
 print('11:app creator')
+print('12:account info')
+print('13:edit password')
 
 while True:
     user_input = input("> ").lower().strip()
@@ -41,16 +45,41 @@ while True:
         print('111111111111111111111111111111')
         print('111111111111111111111111111111')
     elif user_input in ["8", "8"]:
-        input('enter nickname:')
-        input('enter message:')
+        x = input("enter nickname:")
+        u = input('enter message:')
+        print(f"{x}:{u}")
     elif user_input in ["9", "9"]:
-        print('s:song,v:vpn')
+        print('s:song,v:vpn,sp:spam bot')
     elif user_input in ["s", "s"]:
         print('download...')
         print('Last Christmas I gave you my heart But the very next day you gave it away This year To save me from tears I’ll give it to someone special Once bitten and twice shy I keep my distance But you still catch my eye Tell me baby Do you recognize me?')
     elif user_input in ["v", "v"]:
         print('download...')
         print('vpn activated!')
+    elif user_input in ["sp", "sp"]:
+        print('download...')
+        input('press enter')
+        print('spam')
+        print('spam')
+        print('spam')
+        print('spam')
+        print('spam')
+        print('spam')
+        print('spam')
+        print('spam')
+        print('spam')
+        print('spam')
+        print('spam')
+        print('spam')
+        print('spam')
+        print('spam')
+        print('spam')
+        print('spam')
+        print('spam')
+        print('spam')
+        print('spam')
+        print('spam')
+        print('spam')
     elif user_input in ["10", "10"]:
         print('answer:', eval(input()))
     elif user_input in ["11", "11"]:
@@ -72,5 +101,10 @@ while True:
         print('1111111                1111111')
         print('111111111111111111111111111111')
         print('111111111111111111111111111111')
+    elif user_input in ["12", "12"]:
+        print(f"username:{t}")
+        print(f"password:{s}")
+    elif user_input in ["13", "13"]:
+        s = input("enter new password:")
     else:
         print("please enter the number")
